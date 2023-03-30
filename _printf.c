4a0878597a8e46212dbafe993c3ae33da4e20512
RamziHkiri <6257@holbertonstudents.com>
2023-03-30 05:40:15 -0700

    test

--- _printf.c
+++ _printf.c
@@ -3,37 +3,6 @@
 #include<stdio.h>
 #include"main.h"
 /**
- * print_c - print character
- * @params:variadic arguments
- * Return:none
- */
-void print_c(va_list params)
-{
-	char c;
-
-	c = va_arg(params, int);
-	putchar(c);
-}
-/**
- * print_s - print string
- * @params:variadic arguments
- * Return:none
- */
-void print_s(va_list params)
-{
-	int i;
-	char *s;
-
-	s = va_arg(params, char *);
-	if (s)
-	{
-		for (i = 0 ; s[i] != '\0' ; i++)
-		{
-			putchar(s[i]);
-		}
-	}
-}
-/**
  * _printf - same as printf
  *@format: string or the mesage to print
  * Return:none
