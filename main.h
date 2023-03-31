8b4952a2e665244db66b8c56b2992a820cbe2690
RamziHkiri <6257@holbertonstudents.com>
2023-03-31 04:51:32 -0700

    test

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format,...);
-void print_c(va_list params);
-void print_s(va_list params);
+int print_c(va_list params);
+int print_s(va_list params);
 
 typedef struct printer
 {
 char *symb;
-void (*func)(va_list params);
+int (*func)(va_list params);
 } print_by_t;
 #endif
