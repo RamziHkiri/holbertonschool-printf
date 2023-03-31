2bab0126a59b6ff60a3e4df7a30b0367f4b2e2e3
RamziHkiri <6257@holbertonstudents.com>
2023-03-31 06:10:21 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+#include<string.h>
 #include<stdarg.h>
 #include<stdlib.h>
 #include<stdio.h>
@@ -26,24 +27,26 @@ int _printf(const char *format, ...)
 	print_by_t pbt[] = {{"s", print_s}, {"c", print_c}};
 
 	va_start(params, format);
-	while (*format)
+	if (!format || (strlen(format) == 1 && *format == '%'))
+			return (0);
+	else
 	{
-		if (state == 0)
+	va_start(params, format);
+	while (*format)
+	{	if (state == 0)
 			if (*format == '%')
 				state = 1;
 			else
-			{
-				leng++;
+			{	leng++;
 				putchar(*format);
 			}
 		else
-		{
-			if (*format == '%')
+		{if (*format == '%')
 			{	init1(leng);
 				leng++;
 			}
 			else
-			{
+			{	
 				if (*format != 'c' && *format != 's')
 				{	init(format, leng);
 					leng += 2;
@@ -58,5 +61,6 @@ int _printf(const char *format, ...)
 	format++;
 	}
 	va_end(params);
+	}
 return (leng);
 }
