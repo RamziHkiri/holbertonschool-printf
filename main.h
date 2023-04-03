d6d9750e32e525e71cd918db2f5f2beb5d29dd0a
RamziHkiri <6257@holbertonstudents.com>
2023-04-03 02:34:28 -0700

    test

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _printf(const char *format,...);
+int _printf(const char *format, ...);
 int print_c(va_list params);
 int print_s(va_list params);
 
+/**
+ * struct printer - data structure to print
+ * @symb: specifier
+ * @func: function pointer
+ */
 typedef struct printer
 {
 char *symb;
